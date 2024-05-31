c48196ce9af275feb0931a8f0b4133422866d6bd
sejal-sharma08 <sejalshrm08@gmail.com>
2024-05-31 23:56:18 +0530

    logic added when password is not equal to cpassword

--- api/register.php
+++ api/register.php
@@ -11,7 +11,24 @@
     $role = $_POST['role'];
 
     if($password == $cpassword){
-        
+        move_uploaded_file($tmp_name, "../uploads/$image");
+        $insert = mysqli_query($connect, "INSERT INTO user (name, mobile, address, password, photo, role, status, votes) VALUES ('$name', '$mobile', '$address', '$password', '$image', '$role', 0, 0)");
+        if($insert) {
+            echo '
+            <script>
+                alert("Registration successful!");
+                window.location = "../";
+            </script>
+            ';
+        }
+        else{
+            echo '
+            <script>
+                alert("Some error occured!");
+                window.location = "../routes/register.html";
+            </script>
+            ';
+        }
     }
     else{
         echo '
