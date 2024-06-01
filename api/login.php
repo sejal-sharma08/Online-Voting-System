daa7af6675a254f19a55850b7dcf36747772f2c6
sejal-sharma08 <sejalshrm08@gmail.com>
2024-06-01 16:39:06 +0530

    login.php updated

--- api/login.php
+++ api/login.php
@@ -1,9 +1,35 @@
 <?php
-
+session_start();
 include("connect.php");
 
 $mobile = $_POST['mobile'];
 $password = $_POST['password'];
 $role = $_POST['role'];
 
+$check = mysqli_query($connect, "SELECT * FROM user WHERE mobile = '$mobile' AND password = '$password' AND role = '$role'");
+
+if (mysqli_num_rows($check) > 0) {
+    // fetching user data
+    $userdata = mysqli_fetch_array($check);
+    $groups = mysqli_query($connect, "SELECT * FROM user WHERE role = 2");
+    // fetching groups data
+    $groupsdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
+
+    $_SESSION['userdata'] = $userdata;
+    $_SESSION['groups'] = $groupsdata;
+
+    echo '
+            <script>
+                window.location = "../routes.dashboard.php";
+            </script>
+            ';    
+}
+else {
+    echo '
+            <script>
+                alert("Invalif Credentials or User not found!");
+                window.location = "../";
+            </script>
+            ';
+}
 ?>
